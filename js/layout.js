af98e369b447fdf4b4f54758b1e5b570f11891c4
Edward Tymoshuk <martapetrovych@MacBook-Pro-Marta.local>
2019-11-01 22:36:44 +0100

    hide header block, show active menu items

--- js/layout.js
+++ js/layout.js
@@ -12,12 +12,20 @@
 
     window.addEventListener('scroll', function() {
         let headerEl = document.getElementById('header-block');
-        for (let i=0; i<100; i+=10) {
+        let headerBlock = document.getElementById('header-block');
+        let headerPanel = document.getElementById('header-panel');
+        for (let i=0; i<=100; i+=10) {
         if(pageYOffset >= document.documentElement.clientHeight/(100/i)) {
             headerEl.style.background = `rgba(255, 255, 255, 0.${i/10})`;
+            headerPanel.style.opacity = `.${90-i}`;
         }
         if (pageYOffset >= document.documentElement.clientHeight-headerEl.offsetHeight) {
             headerEl.style.background = `rgba(255, 255, 255, 1)`;
+        }
+    }
+    for (let i=0; i<=65; i+=10) {
+        if(pageYOffset >= document.documentElement.clientHeight/(100/i)) {
+            headerBlock.style.top = `-${i}px`;
         }
     }
     });
@@ -28,7 +36,7 @@
             event.preventDefault();
             var id  = $(this).attr('href'),
                 top = $(id).offset().top;
-            $('body,html').animate({scrollTop: top-headerEl.offsetHeight}, 1000);
+            $('body,html').animate({scrollTop: top-headerEl.offsetHeight/3}, 1000);
         });
     });
 
@@ -42,26 +50,6 @@
         });
     });
 
-    $(document).ready(function() {
-
-        //E-mail Ajax Send
-        $("contact-form").submit(function() { //Change
-            var th = $(this);
-            $.ajax({
-                type: "POST",
-                url: "/mail.php",
-                data: th.serialize()
-            }).done(function() {
-                alert("Thank you!");
-                setTimeout(function() {
-                    // Done Functions
-                    th.trigger("reset");
-                }, 1000);
-            });
-            return false;
-        });
-    
-    });
 // let el = document.querySelector('#home');
 // console.log(el);
 // el.addEventListener('click', (e) => {
@@ -73,4 +61,6 @@
 //     let top = document.getElementById(id);
 //     console.log(top);
 // })
+
+
 }())
